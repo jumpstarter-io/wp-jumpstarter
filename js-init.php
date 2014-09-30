1bcc535b4d24011e36bc0b8651c7bace896d29a8
Hannes Landeholm <hannes@jumpstarter.io>
2014-09-30 13:51:08 +0200

    init commit

---
+++ js-init.php
@@ -0,0 +1,260 @@
+<?php
+/*
+ * This Jumpstarter init file should be pre executed before the web server
+ * starts. It indempotently handles automatic wordpress installation if the
+ * app is starting for the first time in an instance.
+ *
+ * You should pre_exec this script before starting the web server by running:
+ * php /app/code/src/wp-content/mu-plugins/jumpstarter/js-auto-install.php
+ */
+
+// Log to stderr.
+function js_log($msg) {
+    fwrite(STDERR, "[js-init.php] $msg\n");
+}
+
+// Execute wrapper that throws exception and leaks output if command fails.
+function js_eexec($cmd) {
+    exec($cmd, $out, $ret);
+    // The execute could have side effects on the file system that PHP
+    // doesn't know about. We need to clear the stat cache.
+    clearstatcache();
+    if ($ret !== 0) {
+        fwrite(STDERR, implode("\n", $out));
+        throw new Exception("executing cmd [$cmd] failed");
+    }
+}
+
+function js_read_env() {
+    $env = json_decode(file_get_contents("/app/env.json"), true);
+    if (!is_array($env))
+        throw new Exception("jumpstarter install failed: could not parse /app/env.json");
+    // It can be useful to test the app ident environment in assemblies.
+    $test_app_env_path = "/app/code/test-app-ident-env.json";
+    if (!isset($env["ident"]["app"]) && is_file($test_app_env_path))
+        $env["ident"]["app"] = json_decode(file_get_contents($test_app_env_path), true);
+    return $env;
+}
+
+function js_db_state_dir() {
+    return "/app/state/wp-db";
+}
+
+function js_include_wp() {
+    // Only include once.
+    static $wp_included = false;
+    if ($wp_included)
+        return;
+
+    // Include wordpress from CLI.
+    define("ABSPATH", realpath(__DIR__ . "/../../../") . "/");
+    $_SERVER['HTTPS'] = "on";
+    js_log("including wp-load.php");
+    require_once(ABSPATH . "wp-load.php");
+    js_log("including upgrade.php");
+    require_once(ABSPATH . "wp-admin/includes/upgrade.php");
+
+    // Dev sanity check: Ensure that DISALLOW_FILE_MODS is set to the right value.
+    if (!defined("DISALLOW_FILE_MODS") || DISALLOW_FILE_MODS !== true) {
+        js_log("error: DISALLOW_FILE_MODS is not set to the correct value in wp-config.php, it should be set to [true]");
+        exit;
+    }
+
+    // Dev sanity check: Ensure that DB_DIR is set to the right value.
+    $db_state_dir = js_db_state_dir();
+    if (!defined("DB_DIR") || DB_DIR !== $db_state_dir) {
+        js_log("error: DB_DIR is not set to the correct value in wp-config.php, it should be set to [$db_state_dir]");
+        exit;
+    }
+
+    // Dev sanity check: Ensure that there is no database folder or symlink.
+    $bad_db_path = (ABSPATH . "wp-content/database");
+    if (file_exists($bad_db_path) || is_link($bad_db_path)) {
+        js_log("error: the path [$bad_db_path] exists, it's a security hazard and should be removed");
+        exit;
+    }
+
+    // Wordpress is included now.
+    $wp_included = true;
+}
+
+function js_install_wp() {
+    $db_state_dir = js_db_state_dir();
+    // Create symlink to allow extremly fast ram based install.
+    $db_tmp_dir = "/tmp/wp-db";
+    if (file_exists($db_tmp_dir))
+        js_eexec("rm -rf " . escapeshellarg($db_tmp_dir));
+    js_eexec("mkdir " . escapeshellarg($db_tmp_dir));
+    js_eexec("ln -s " . escapeshellarg($db_tmp_dir) . " " . escapeshellarg($db_state_dir));
+
+    // When exiting before succesfull install, return bad error code.
+    $install_ok = false;
+    register_shutdown_function(function() use (&$install_ok) {
+        if (!$install_ok)
+            exit(1);
+    });
+
+    // Read and prepare configuration for automatic install.
+    $env = js_read_env();
+    $is_assembly = !empty($env["ident"]["container"]["is_assembly"]);
+    $blog_title = "My blog";
+    $user_name = empty($env["ident"]["user"]["name"])? "admin": strval($env["ident"]["user"]["name"]);
+    $user_email = strval($env["ident"]["user"]["email"]);
+    $public = true;
+    $deprecated = null;
+    // Let wordpress generate a random password for instances, for assemblies, use "test".
+    $user_password = $is_assembly? "test": null;
+    $language = null;
+
+    // We pretend a user visited wordpress to install it through the generated domain.
+    define("WP_SITEURL", js_get_env_siteurl($env));
+    define("WP_INSTALLING", true);
+
+    // Include wordpress definitions and config.
+    js_include_wp();
+
+    // Install wordpress now.
+    js_log("running wordpress installer with name:[$user_name], email:[$user_email], password:[$user_password]");
+    wp_install($blog_title, $user_name, $user_email, $public, $deprecated, $user_password, $language);
+
+    // Set capabilites.
+    js_log("setting capabilities of root user");
+    $super_admins = get_super_admins();
+    if (count($super_admins) != 1)
+        throw new Exception("unexpected super admin count after install");
+    $user_login = array_pop($super_admins);
+    $user = get_user_by('login', $user_login);
+    $user->remove_cap("switch_themes");
+
+    // Silently activate all required plugins.
+    // The developer should have removed plugins that should not be activated.
+    js_log("activate plugins");
+    foreach (array("jumpstarter/jumpstarter.php", "sqlite-integration/sqlite-integration.php") as $plugin_key) {
+        js_log("activating plugin [$plugin_key]");
+        $result = activate_plugin($plugin_key, '', false, true);
+        if ($result !== null) {
+            js_log("failed to activate plugin!\n" . json_encode($result->get_error_messages()));
+            exit;
+        }
+    }
+
+    // Copy the database over to the state and atomically move it in place to mark wordpress as installed.
+    // We delete any old temporary db state to make the install indempotent.
+    js_log("copying installed database from ram to state");
+    $db_state_tmp_dir = "/app/state/wp-db.tmp";
+    if (file_exists($db_state_tmp_dir))
+        js_eexec("rm -rf " . escapeshellarg($db_state_tmp_dir));
+    js_eexec("cp -rp ". escapeshellarg($db_tmp_dir) . " " . escapeshellarg($db_state_tmp_dir));
+    js_eexec("rm -rf " . escapeshellarg($db_tmp_dir));
+    js_eexec("rm " . escapeshellarg($db_state_dir));
+    js_log("syncing data to state");
+    js_eexec("sync");
+    js_log("final atomic move");
+    js_eexec("mv ". escapeshellarg($db_state_tmp_dir) . " " . escapeshellarg($db_state_dir));
+
+    // Wait for sync before considering installation complete.
+    js_eexec("sync");
+    $install_ok = true;
+    js_log("succesfully installed wordpress!");
+
+    // Restart the CLI script to run the init phase again to test with an installed wordpress and sync env.
+    global $argv;
+    $execve_path = PHP_BINARY;
+    $execve_args = $argv;
+    js_log("****** restarting: [$execve_path], [" . implode(", ", $execve_args) . "] ******");
+    pcntl_exec($execve_path, $execve_args);
+    exit(1);
+}
+
+function js_update_siteurl($old_siteurl, $new_siteurl) {
+    global $wpdb;
+    $wpdb->query($wpdb->prepare("UPDATE wp_posts SET post_content = replace(post_content, %s, %s)", $old_siteurl, $new_siteurl));
+    update_option("siteurl", $new_siteurl);
+    update_option("home", $new_siteurl);
+}
+
+function js_get_env_siteurl($env) {
+    // Primarily use top user domain if one is configured.
+    $user_domains = isset($env["settings"]["core"]["user-domains"])? $env["settings"]["core"]["user-domains"]: array();
+    if (is_array($user_domains) && count($user_domains) > 0) {
+        $preferred = reset($user_domains);
+        foreach ($user_domains as $domain) {
+            if ($domain["preferred"]) {
+                $preferred = $domain;
+                break;
+            }
+        }
+        if (empty($preferred["name"]))
+            throw new Exception("corrupt env: preferred domain has no name");
+        return ($domain["secure"]? "https": "http") . "://" . $preferred["name"];
+    }
+    // Fall back to auto domain (always encrypted).
+    if (empty($env["settings"]["core"]["auto-domain"]))
+        throw new Exception("auto-domain not found in env");
+    return "https://" . $env["settings"]["core"]["auto-domain"];
+}
+
+function js_get_env_theme($env) {
+    if (!isset($env["ident"]["app"]["extra_env"]["theme"]))
+        return null;
+    return $env["ident"]["app"]["extra_env"]["theme"];
+}
+
+function js_sync_wp_with_env() {
+    js_log("starting env sync");
+    $env = js_read_env();
+    // Include wordpress definitions and config.
+    js_include_wp();
+    // Sync wordpress domain if it changed.
+    $wp_siteurl = get_option("siteurl");
+    $env_siteurl = js_get_env_siteurl($env);
+    if ($wp_siteurl !== $env_siteurl) {
+        js_log("siteurl change detected, migrating from [$wp_siteurl] to [$env_siteurl]");
+        js_update_siteurl($wp_siteurl, $env_siteurl);
+    } else {
+        js_log("no siteurl change detected, keeping [$wp_siteurl]");
+    }
+    // Apply wordpress theme from env.
+    $stylesheet = js_get_env_theme($env);
+    if (is_string($stylesheet)) {
+        js_log("setting theme [$stylesheet]");
+        $theme = wp_get_theme($stylesheet);
+        if (!$theme->exists())
+            throw new Exception("theme to install [$stylesheet] not found!");
+        switch_theme($theme->get_stylesheet());
+    } else {
+        js_log("no theme specified in env, nothing to set");
+    }
+    js_log("completed env sync");
+}
+
+call_user_func(function() {
+    // We don"t want to run unless invoked by cli.
+    if (php_sapi_name() !== "cli")
+        return;
+
+    // Check for previous failed installation.
+    $db_state_dir = js_db_state_dir();
+    if (is_link($db_state_dir)) {
+        js_log("found old [$db_state_dir] symlink, assuming previous failed installation");
+        js_eexec("rm " . escapeshellarg($db_state_dir));
+    }
+
+    // We don"t want to install if already installed.
+    $throw_invalid_inode_type_fn = function() use ($db_state_dir) {
+        throw new Exception("invalid inode type for path [$db_state_dir] (expected directory)");
+    };
+    if (file_exists($db_state_dir)) {
+        if (!is_dir($db_state_dir))
+            $throw_invalid_inode_type_fn();
+        js_log("skipping wordpress install (already done)");
+    } else {
+        if (is_link($db_state_dir))
+            $throw_invalid_inode_type_fn();
+        js_log("installing wordpress (first run)");
+        js_install_wp();
+    }
+
+    // Enforce wordpress configuration by environment.
+    js_sync_wp_with_env();
+});
---
+++ jumpstarter.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Plugin Name: Jumpstarter
+ * Plugin URI: https://github.com/jumpstarter-io/
+ * Description: Wordpress Jumpstarter integration.
+ * Version: 0.1
+ * Author: Jumpstarter
+ * Author URI: https://jumpstarter.io/
+ * License: Public Domain
+ */
+
+// Don't load directly.
+if (!defined("ABSPATH"))
+    die("-1");
+
+// Prevent deactivation of required plugins.
+add_action("deactivate_plugin", function($plugin_key) {
+    if ($plugin_key === "jumpstarter/jumpstarter.php"
+    || $plugin_key === "sqlite-integration/sqlite-integration.php") {
+        wp_die(__("This plugin cannot be deactivated!"));
+    }
+});
+
+// Sandboxed Jumpstarter Wordpress user.
+class JS_WP_User extends WP_User {
+    public function __construct(WP_User $raw_wp_user) {
+        foreach (get_object_vars($raw_wp_user) as $key => $var)
+            $this->$key = $var;
+    }
+
+    public function has_cap($in) {
+        $cap = (is_numeric($in)? $this->translate_level_to_cap($cap): $in);
+        switch ($cap) {
+        // We don't allow switching themes. The theme is defined
+        // by the app in jumpstarter and set when installing.
+        case "switch_themes":
+            return false;
+        }
+        return parent::has_cap($in);
+    }
+}
+
+// Validates the cookie z with the session id x.
+function js_auth_verify($x, $z) {
+    // Sanity check input.
+    if (!is_string($z) || !is_string($x))
+        return false;
+    if (strlen($z) != 144 || strlen($x) != 64)
+        return false;
+    if (!ctype_xdigit($z) || !ctype_xdigit($x))
+        return false;
+
+    // Decode all cookie parameters.
+    $z_raw = hex2bin($z);
+    $e_raw = substr($z_raw, 0, 8);
+    $e = implode(unpack("N", substr($e_raw, 4, 4))); // expire time
+    $y = substr($z_raw, 8, 32); // random salt
+    $h = substr($z_raw, 40); // sha256 hash signature
+
+    // Cookie must not have expired.
+    if ($e < time())
+        return false;
+
+    // Calculate the signature we expect.
+    $h_challenge = hash("sha256", ($e_raw . $y . hex2bin($x)), true);
+
+    // Sanity check before comparing.
+    if (!is_string($h) || strlen($h) != 32)
+        return false;
+    if (!is_string($h_challenge) || strlen($h_challenge) != 32)
+        return false;
+
+    // Final authorization.
+    return ($h === $h_challenge);
+}
+
+function js_auth_get_x() {
+    if (!file_exists("/app/env.json"))
+        return null;
+    $json = json_decode(file_get_contents("/app/env.json"), true);
+    return $json["ident"]["container"]["session_key"];
+}
+
+add_action('login_init', function() {
+    // Attempt to login automatically via jumpstarter token at /wp-login.php?autologin=js.
+    if (!isset($_GET["autologin"]) || $_GET["autologin"] !== "js")
+        return;
+    if (!isset($_POST['token']))
+        wp_die(__("Jumpstarter login failed: no login token in POST."));
+    $z = $_POST["token"];
+    $x = js_auth_get_x();
+    if (!js_auth_verify($x, $z))
+        wp_die(__("Jumpstarter login failed: authorization failed (old token?)."));
+    foreach (get_super_admins() as $admin_login) {
+        $user = get_user_by('login', $admin_login);
+        if (is_object($user)) {
+            wp_set_auth_cookie($user->ID);
+            wp_redirect(admin_url());
+            exit;
+        }
+    }
+    wp_die(__("Jumpstarter login failed: no valid account found."));
+});
+
+add_action("login_footer", function () {
+    ?>
+        <div id="js-login" style="clear: both; padding-top: 20px; margin-bottom: -15px;">
+            <a href="https://jumpstarter.io/reflected-login">Login with Jumpstarter</a>
+        </div>
+        <script type="text/javascript">
+            var jsl = document.getElementById("js-login");
+            var lgf = document.getElementById("loginform");
+            lgf.appendChild(jsl);
+        </script>
+    <?php
+});
+
+add_action("set_current_user", function() {
+    global $current_user;
+    if (!is_object($current_user))
+        return;
+    // Sandbox the current user per the Jumpstarter environment.
+    $current_user = new JS_WP_User($current_user);
+});
+
+// Short-circuit URLs to emulate hard coded configuration.
+define("WP_SITEURL", get_option("siteurl"));
+define("WP_HOME", get_option("home"));
