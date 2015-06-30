2b5b6299ed3a39b855722e225ba04c06c357f6a6
Stefan Nyman <stefan@jumpstarter.io>
2015-06-30 13:48:54 +0200

    jswp-env: remove functionality that's present in js-php-env project and use that instead.

--- jswp-env.php
+++ jswp-env.php
@@ -2,59 +2,23 @@
 
 require_once("/app/code/js-php-env/js-env.php");
 
+define("JSWP_ENV_PATH", "/app/code/wp-env.json");
+define("JSWP_ENV_APC_KEY", "wp-env");
+
 function jswp_get_env() {
-    // Fetch parsed environment from request cache.
-    static $env = null;
-    if ($env !== null)
-        return $env;
-    // Fetch parsed environment from server cache.
-    if (function_exists("apc_fetch")) {
-        $key = "wp-env";
-        $env = apc_fetch($key);
-        if (is_array($env))
-            return $env;
-    }
-    // Read environment and parse it.
-    $env = json_decode(file_get_contents("/app/code/wp-env.json"), true);
-    if (!is_array($env))
-        $env = array();
-    // Store parsed environment in server cache.
-    if (function_exists("apc_store")) {
-        $key = "wp-env";
-        apc_store($key, $env);
-    }
-    return $env;
+    return js_get_env(JSWP_ENV_PATH, JSWP_ENV_APC_KEY);
 }
 
-function jswp_env_get_value($path) {
-    static $cache = array();
-    $env = jswp_get_env();
-    if (isset($cache[$path]))
-        return $cache[$path];
-    $path_arr = explode(".", $path);
-    if (count($path_arr) == 0)
-        return null;
-    if (!is_array($env))
-        return null;
-    $obj = $env;
-    foreach($path_arr as $part) {
-        if (!isset($obj[$part])) {
-            $cache[$path] = null;
-            return null;
-        }
-        $obj = $obj[$part];
-    }
-    $cache[$path] = $obj;
-    return $obj;
+function jswp_env_get_value($key_path) {
+    return js_env_get_value($key_path, JSWP_ENV_PATH, JSWP_ENV_APC_KEY);
+}
+
+function jswp_env_get_val_or_array($path) {
+    return js_env_get_value_or_array($path, JSWP_ENV_PATH, JSWP_ENV_APC_KEY);
 }
 
 function jswp_env_get_theme() {
     return jswp_env_get_value("theme");
-}
-
-function jswp_env_get_val_or_array($path) {
-    $obj = jswp_env_get_value($path);
-    return is_array($obj)? $obj: array();
 }
 
 function jswp_env_get_plugins() {
