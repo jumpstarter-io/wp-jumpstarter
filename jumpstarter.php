6faa7cec0ba378af3bff847a29102eb5f7126372
Stefan Nyman <stefan@jumpstarter.io>
2015-08-12 19:46:20 +0200

    jumpstarter: update version tag

--- jumpstarter.php
+++ jumpstarter.php
@@ -5,7 +5,7 @@
  * Description: Jumpstarter WordPress integration plugin that simplifies running WordPress in a container environment.
  * Plugin Name: Jumpstarter
  * Plugin URI: https://github.com/jumpstarter-io/wp-jumpstarter
- * Version: 16.5
+ * Version: 17.0
  * License: Unlicense
  * License URI: http://unlicense.org
  */
