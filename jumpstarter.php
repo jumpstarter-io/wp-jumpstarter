9f296fd53ad1c9666a408ad2a0d5998926cb29ca
Stefan Nyman <stefan@jumpstarter.io>
2015-09-14 15:14:45 +0200

    version bump

--- jumpstarter.php
+++ jumpstarter.php
@@ -5,7 +5,7 @@
  * Description: Jumpstarter WordPress integration plugin that simplifies running WordPress in a container environment.
  * Plugin Name: Jumpstarter
  * Plugin URI: https://github.com/jumpstarter-io/wp-jumpstarter
- * Version: 17.3
+ * Version: 18
  * License: Unlicense
  * License URI: http://unlicense.org
  */
